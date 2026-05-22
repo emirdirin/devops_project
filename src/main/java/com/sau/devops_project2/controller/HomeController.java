c8a5d9ad155b5fab13ddb7f2f2048a56db08034d
emir dirin <emir.dirin@ogr.sakarya.edu.tr>
2026-05-22 11:18:48 +0300

    Jenkins pipeline test.

--- src/main/java/com/sau/devops_project2/controller/HomeController.java
+++ src/main/java/com/sau/devops_project2/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home(){
-        return " This is the home page of the application";
+        return " This is the home page of the application. Jenkins pipeline worked successfully! -----";
     }
 }
